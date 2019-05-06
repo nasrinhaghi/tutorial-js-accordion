527a1e698a8a84533fbd58d86186569192b4e9e6
nasrin <n1.haghi@gmail.com>
2019-05-06 22:03:27 +0430

    step1

--- src/js/jsaccordion.js
+++ src/js/jsaccordion.js
@@ -3,8 +3,38 @@ function JSAccordion(elementOrSelector) {
         return new JSAccordion(elementOrSelector);
 
     //  define public methods
-    this.test = function(color) {
-        this.targetElement.style.color = color;
+    this.init = function() {
+    };
+
+    //  start construction operations
+    //  if parameter is element selector
+    if(typeof elementOrSelector == 'string') {
+        this.targetElement = document.querySelector(elementOrSelector);
+        if(this.targetElement == null) {
+            throw ('invalid element selector');
+        }
+    }
+    //  if parameter is element DOM object
+    else if(typeof elementOrSelector == 'object')
+        this.targetElement = elementOrSelector;
+    else
+        throw ('Unknown element type');
+
+    //  set autoincrement instance id to object
+    this.id = JSAccordion.instances.length;
+
+    JSAccordion.instances.push(this);
+
+    return this;
+}
+
+//  define static property to keep all instances
+JSAccordion.instances = [];function JSAccordion(elementOrSelector) {
+    if(!(this instanceof JSAccordion))
+        return new JSAccordion(elementOrSelector);
+
+    //  define public methods
+    this.innit = function() {
     };
 
     //  start construction operations
